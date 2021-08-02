944288924c588922e7c81b760a0222255f6af9f1
namoo <namoo.developer@gmail.com>
2021-08-02 15:50:05 +0900

    Update introduction message

--- src/component/pages/IntroPage/index.js
+++ src/component/pages/IntroPage/index.js
@@ -28,11 +28,11 @@ function IntroPage(props)
                 <IconButton icon={IoLogoInstagram} onClick={openInstagram}/>
             </LinkLayout>
             <Introduction>
-                현재 컴퓨터과학과에 재학중이며 코딩에 흥미를 가지고 열심히 공부하고 있습니다 :)<br/>
-                안드로이드 앱 개발과 더불어 React를 이용한 웹 개발을 도전하고 있습니다.<br/>
-                특히 UI/UX에 관심이 많으며 심심할 때 그림도 그립니다!<br/>
-                '프로젝트'를 눌러 지금까지 직접 참여한 프로젝트들을 확인할 수 있습니다.<br/>
-                정리 및 공유를 위한 블로그도 운영하고 있으니 한 번씩 구경해주세요~
+                현재 컴퓨터과학과에 재학중이며 코딩에 흥미를 가지고 열심히 공부하고 있습니다.<br/>
+                앱/웹/프로그램 개발 경험이 있으며 특히 UI/UX에 관심이 많습니다!<br/>
+                최근에는 알고리즘 공부로 고통받고 있습니다 ㅜㅜ<br/>
+                '프로젝트'를 눌러 지금까지 참여한 프로젝트들을 확인할 수 있습니다.<br/>
+                추가로 블로그도 운영하고 있으니 한 번씩 구경해주세요 :)
             </Introduction>
             <LinkLayout>
                 <SimpleButton onClick={() => pushLink("/blog")}>블로그</SimpleButton>
