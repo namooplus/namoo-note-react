5212cea84a897ee087c137014286f91365481c3d
namoo <namoo.developer@gmail.com>
2021-09-19 15:51:18 +0900

    Refactor : Change tab spaces on App.js

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ import { Container, Group } from "./App.component";
 
 function App() 
 {
-  const [isHeaderCollapsed, setHeaderCollapsed] = useState(true);
-  const currentLocation = useLocation();
+    const [isHeaderCollapsed, setHeaderCollapsed] = useState(true);
+    const currentLocation = useLocation();
 
-  return (
-    <Container.Base>
-      {/* 헤더 */}
-      <Group.Header collapsed={isHeaderCollapsed} setCollapsed={setHeaderCollapsed} currentPath={currentLocation.pathname}/>
-      {/* 내용 */}
-      <Switch>
-        <Route exact path="/" component={IntroPage}/>
-        <Route exact path="/blog" component={() => <ListPage type="blog"/>}/>
-        <Route exact path="/project" component={() => <ListPage type="project"/>}/>
-        <Route exact path="/post/:postId" component={PostPage}/>
-        <Route exact path="/error" component={ErrorPage}/>
-        <Route component={() => <Redirect to="/error"/>}/>
-      </Switch>
-    </Container.Base>
-  );
+    return (
+        <Container.Base>
+            {/* 헤더 */}
+            <Group.Header collapsed={isHeaderCollapsed} setCollapsed={setHeaderCollapsed} currentPath={currentLocation.pathname}/>
+            {/* 내용 */}
+            <Switch>
+                <Route exact path="/" component={IntroPage}/>
+                <Route exact path="/blog" component={() => <ListPage type="blog"/>}/>
+                <Route exact path="/project" component={() => <ListPage type="project"/>}/>
+                <Route exact path="/post/:postId" component={PostPage}/>
+                <Route exact path="/error" component={ErrorPage}/>
+                <Route component={() => <Redirect to="/error"/>}/>
+            </Switch>
+        </Container.Base>
+    );
 }
 
 export default App;
